d00fbca09472ae68c424deec1c3469f1cb20820e
Gregory Szorc <gregory.szorc@gmail.com>
2019-08-04 17:38:47 -0700

    linux: tcl build improvements
    No longer build shared libraries for packages. Also trim out
    some packages that we don't need or want.
    
    This gets the tcl package building on musl. But we're still
    broken building tk...

--- cpython-linux/build-tcl.sh
+++ cpython-linux/build-tcl.sh
@@ -11,7 +11,28 @@ export PATH=/tools/${TOOLCHAIN}/bin:/tools/host/bin:$PATH
 export PKG_CONFIG_PATH=/tools/deps/share/pkgconfig:/tools/deps/lib/pkgconfig
 
 tar -xf tcl8.6.9-src.tar.gz
-pushd tcl8.6.9/unix
+pushd tcl8.6.9
+
+patch -p1 << 'EOF'
+diff --git a/unix/Makefile.in b/unix/Makefile.in
+--- a/unix/Makefile.in
++++ b/unix/Makefile.in
+@@ -1724,7 +1724,7 @@ configure-packages:
+ 		  $$i/configure --with-tcl=../.. \
+ 		      --with-tclinclude=$(GENERIC_DIR) \
+ 		      $(PKG_CFG_ARGS) --libdir=$(PACKAGE_DIR) \
+-		      --enable-shared --enable-threads; ) || exit $$?; \
++		      --enable-shared=no --enable-threads; ) || exit $$?; \
+ 	      fi; \
+ 	    fi; \
+ 	  fi; \
+EOF
+
+# Remove packages we don't care about and can pull in unwanted symbols.
+rm -rf pkgs/sqlite* pkgs/tdbc*
+
+pushd unix
+
 CFLAGS="-fPIC -I/tools/deps/include" ./configure \
     --prefix=/tools/deps \
     --enable-shared=no
