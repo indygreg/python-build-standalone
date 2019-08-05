d1fda874a76ded3c27ec6faf85fb089b3225b94d
Gregory Szorc <gregory.szorc@gmail.com>
2019-08-04 18:09:19 -0700

    linux: hack up tk to build on musl
    We were getting undefined symbols trying to link wish even when
    we used a static library containing those symbols in text
    sections. I'm not sure what's going on.
    
    This patch works around the problem by disabling wish, which we
    shouldn't need anyway.

--- cpython-linux/build-tk.sh
+++ cpython-linux/build-tk.sh
@@ -20,5 +20,19 @@ CFLAGS="-fPIC" ./configure \
     --with-tcl=/tools/deps/lib \
     --enable-shared=no
 
+# For some reason musl isn't link libXau and libxcb. So we hack the Makefile
+# to do what we want.
+#
+# In addition, the wish binary is also failing to link. So we remove it
+# from the build and the installation (it shouldn't be needed anyway).
+if [ "${CC}" = "musl-clang" ]; then
+    sed -i 's/-ldl  -lpthread /-ldl  -lpthread -lXau -lxcb/' tkConfig.sh
+    sed -i 's/-lpthread $(X11_LIB_SWITCHES) -ldl  -lpthread/-lpthread $(X11_LIB_SWITCHES) -ldl  -lpthread -lXau -lxcb/' Makefile
+    sed -i 's/all: binaries libraries doc/all: libraries/' Makefile
+    sed -i 's/install-binaries: $(TK_STUB_LIB_FILE) $(TK_LIB_FILE) ${WISH_EXE}/install-binaries: $(TK_STUB_LIB_FILE) $(TK_LIB_FILE)/' Makefile
+fi
+
 make -j `nproc`
+touch wish
 make -j `nproc` install DESTDIR=/build/out
+rm /build/out/tools/deps/bin/wish*
