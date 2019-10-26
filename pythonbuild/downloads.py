171378f46aff800ae1c8170ff7bb5fea4366e9c6
Gregory Szorc <gregory.szorc@gmail.com>
2019-10-25 20:54:11 -0700

    downloads: run black

--- pythonbuild/downloads.py
+++ pythonbuild/downloads.py
@@ -20,7 +20,7 @@ DOWNLOADS = {
         "version": "2.32",
     },
     "bzip2": {
-        'url': 'https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz',
+        "url": "https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz",
         "size": 810029,
         "sha256": "ab5a03176ee106d3f0fa90e381da478ddae405918153cca248e682cd0c4a2269",
         "version": "1.0.8",
