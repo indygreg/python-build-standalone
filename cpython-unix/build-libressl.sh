84fc7d1f073e6afc41c714e165ce4268ef38843a
Gregory Szorc <gregory.szorc@gmail.com>
2019-10-25 23:04:00 -0700

    unix: remove libressl patch
    This patch is no longer needed as it is incorporated in the
    version we're now using.

--- cpython-unix/build-libressl.sh
+++ cpython-unix/build-libressl.sh
@@ -13,25 +13,6 @@ tar -xf libressl-${LIBRESSL_VERSION}.tar.gz
 
 pushd libressl-${LIBRESSL_VERSION}
 
-# Backport of https://github.com/libressl-portable/portable/pull/529 for MUSL support.
-patch -p1 << EOF
-diff --git a/crypto/compat/getprogname_linux.c b/crypto/compat/getprogname_linux.c
-index 2c89743..4e7e31f 100644
---- a/crypto/compat/getprogname_linux.c
-+++ b/crypto/compat/getprogname_linux.c
-@@ -26,9 +26,7 @@ getprogname(void)
- #if defined(__ANDROID_API__) && __ANDROID_API__ < 21
- 	extern const char *__progname;
- 	return __progname;
--#elif defined(__GLIBC__)
--	return program_invocation_short_name;
- #else
--#error "Cannot emulate getprogname"
-+	return program_invocation_short_name;
- #endif
- }
-EOF
-
 CFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" CPPFLAGS="${EXTRA_TARGET_CFLAGS} -fPIC" ./configure \
     --build=${BUILD_TRIPLE} \
     --host=${TARGET_TRIPLE} \
