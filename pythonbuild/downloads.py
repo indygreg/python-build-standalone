a8761b3473a8154d2c5e19aca0c58ee20dc50d7b
Gregory Szorc <gregory.szorc@gmail.com>
2020-08-22 16:32:41 -0700

    downloads: upgrade binutils to 2.35

--- pythonbuild/downloads.py
+++ pythonbuild/downloads.py
@@ -14,10 +14,10 @@ DOWNLOADS = {
         "license_file": "LICENSE.bdb.txt",
     },
     "binutils": {
-        "url": "ftp://ftp.gnu.org/gnu/binutils/binutils-2.34.tar.xz",
-        "size": 21637796,
-        "sha256": "f00b0e8803dc9bab1e2165bd568528135be734df3fabf8d0161828cd56028952",
-        "version": "2.34",
+        "url": "ftp://ftp.gnu.org/gnu/binutils/binutils-2.35.tar.xz",
+        "size": 22042160,
+        "sha256": "1b11659fb49e20e18db460d44485f09442c8c56d5df165de9461eb09c8302f85",
+        "version": "2.35",
     },
     "bzip2": {
         "url": "https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz",
