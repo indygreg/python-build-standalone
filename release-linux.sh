9c3cbc9609e9e73dcab188926d073653477dbf21
Gregory Szorc <gregory.szorc@gmail.com>
2020-10-05 17:21:18 -0700

    unix: remove Python 3.7 from release script
    We can still maintain support for building this version. But with the
    release of 3.9, I don't want to bloat the release process any more than
    is necessary. Let's only publish 3.8 and 3.9 going forward.

--- release-linux.sh
+++ release-linux.sh
@@ -7,12 +7,8 @@ set -eo pipefail
 
 export PYBUILD_RELEASE_TAG=$(date --utc '+%Y%m%dT%H%M')
 
-./build-linux.py --target-triple x86_64-unknown-linux-gnu --python cpython-3.7 --optimizations debug --libressl
-./build-linux.py --target-triple x86_64-unknown-linux-gnu --python cpython-3.7 --optimizations pgo --libressl
 ./build-linux.py --target-triple x86_64-unknown-linux-gnu --python cpython-3.8 --optimizations debug --libressl
 ./build-linux.py --target-triple x86_64-unknown-linux-gnu --python cpython-3.8 --optimizations pgo --libressl
 
-./build-linux.py --target-triple x86_64-unknown-linux-musl --python cpython-3.7 --optimizations debug --libressl
-./build-linux.py --target-triple x86_64-unknown-linux-musl --python cpython-3.7 --optimizations noopt --libressl
 ./build-linux.py --target-triple x86_64-unknown-linux-musl --python cpython-3.8 --optimizations debug --libressl
 ./build-linux.py --target-triple x86_64-unknown-linux-musl --python cpython-3.8 --optimizations noopt --libressl
--- release-macos.sh
+++ release-macos.sh
@@ -7,7 +7,5 @@ set -eo pipefail
 
 export PYBUILD_RELEASE_TAG=$(date -u '+%Y%m%dT%H%M')
 
-./build-macos.py --python cpython-3.7 --optimizations debug
-./build-macos.py --python cpython-3.7 --optimizations pgo
 ./build-macos.py --python cpython-3.8 --optimizations debug
 ./build-macos.py --python cpython-3.8 --optimizations pgo
