c4cd96e5d26734825314024a109f57d50ae82de6
Gregory Szorc <gregory.szorc@gmail.com>
2021-01-02 12:17:04 -0700

    rust: move mach-o structs to own module
    So code is cleaner.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+mod macho;
+
 use {
+    crate::macho::*,
     anyhow::{anyhow, Context, Result},
     clap::{App, AppSettings, Arg, ArgMatches, SubCommand},
     goblin::mach::load_command::CommandVariant,
     lazy_static::lazy_static,
     scroll::Pread,
     std::{
-        convert::{TryFrom, TryInto},
+        convert::TryInto,
         io::Read,
         path::{Path, PathBuf},
-        str::FromStr,
     },
 };
 
@@ -104,59 +106,6 @@ lazy_static! {
         ]
         .to_vec()
     };
-}
-
-#[derive(Clone, Debug, PartialEq, PartialOrd)]
-struct MachOPackedVersion {
-    value: u32,
-}
-
-impl TryFrom<&str> for MachOPackedVersion {
-    type Error = anyhow::Error;
-
-    fn try_from(value: &str) -> Result<Self, Self::Error> {
-        let parts = value.split('.').collect::<Vec<_>>();
-
-        if parts.len() != 3 {
-            return Err(anyhow!("packed version must have 3 components"));
-        }
-
-        let major = u32::from_str(parts[0])?;
-        let minor = u32::from_str(parts[1])?;
-        let subminor = u32::from_str(parts[2])?;
-
-        let value = (major << 16) | ((minor & 0xff) << 8) | (subminor & 0xff);
-
-        Ok(Self { value })
-    }
-}
-
-impl From<u32> for MachOPackedVersion {
-    fn from(value: u32) -> Self {
-        Self { value }
-    }
-}
-
-impl std::fmt::Display for MachOPackedVersion {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let major = self.value >> 16;
-        let minor = (self.value >> 8) & 0xff;
-        let subminor = self.value & 0xff;
-
-        f.write_str(&format!("{}.{}.{}", major, minor, subminor))
-    }
-}
-
-/// Describes a mach-o dylib that can be loaded by a distribution.
-#[derive(Clone, Debug, PartialEq)]
-struct MachOAllowedDylib {
-    /// Name of the dylib.
-    ///
-    /// Typically an absolute filesystem path.
-    name: String,
-
-    /// Maximum compatibility version that can be referenced.
-    max_compatibility_version: MachOPackedVersion,
 }
 
 fn validate_macho(path: &Path, macho: &goblin::mach::MachO, bytes: &[u8]) -> Result<()> {
