3939c0ee2fb9fa3d7c5532f5fb7c28dd931d069b
Gregory Szorc <gregory.szorc@gmail.com>
2021-02-22 12:45:38 -0800

    downloads: fetch libffi source from github.com
    The previous site has failed intermittently a few times in the past
    week. Let's use a hopefully more reliable mirror.

--- pythonbuild/downloads.py
+++ pythonbuild/downloads.py
@@ -156,7 +156,7 @@ DOWNLOADS = {
         "license_file": "LICENSE.libedit.txt",
     },
     "libffi": {
-        "url": "ftp://sourceware.org/pub/libffi/libffi-3.3.tar.gz",
+        "url": "https://github.com/libffi/libffi/releases/download/v3.3/libffi-3.3.tar.gz",
         "size": 1305466,
         "sha256": "72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056",
         "version": "3.3",
