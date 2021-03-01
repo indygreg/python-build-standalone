25a07d396c1a035c620433e3eeeaa9c691b05eec
Gregory Szorc <gregory.szorc@gmail.com>
2021-03-01 08:20:15 -0800

    unix: improve ncurses cross-compiling
    Previously, cross-compiles only worked on Apple and x86_64 -> i686.
    Upcoming commits to introduce ARM Linux cross-compiles uncovered more
    issues.
    
    This commit contains changes to work around those.
    
    We have to save and restore CC to get the host build to use the
    correct compiler.
    
    We also need to munge the path to the host compiler to use the host
    GCC present in the new cross-compile environment.
    
    And we need to disable application stripping, since `strip` doesn't
    recognize the target. This is probably the hackiest change. But I
    couldn't figure out (only spent minimal effort though) on how to
    get ncurses's build system to use the correct `strip` binary.

--- cpython-unix/build-ncurses.sh
+++ cpython-unix/build-ncurses.sh
@@ -19,10 +19,15 @@ tar -xf ncurses-${NCURSES_VERSION}.tar.gz
 if [[ "${BUILD_TRIPLE}" != "${TARGET_TRIPLE}" && "${PYBUILD_PLATFORM}" != "macos" ]]; then
   echo "building host ncurses to provide modern tic for cross-compile"
 
+  OLD_CC=${CC}
+  unset CC
+
   pushd ncurses-${NCURSES_VERSION}
   ./configure --prefix=${TOOLS_PATH}/host --without-cxx --without-tests --without-manpages --enable-widec
   make -j ${NUM_CPUS}
   make -j ${NUM_CPUS} install
+
+  export CC=${OLD_CC}
 
   popd
 
@@ -33,6 +38,11 @@ fi
 
 pushd ncurses-${NCURSES_VERSION}
 
+# `make install` will strip installed programs (like tic) by default. This is
+# fine. However, cross-compiles can run into issues where `strip` doesn't
+# recognize the target architecture. We could fix this by overriding strip.
+# But we don't care about the installed binaries, so we simply disable
+# stripping of the binaries.
 CONFIGURE_FLAGS="
     --build=${BUILD_TRIPLE}
     --host=${TARGET_TRIPLE}
@@ -40,11 +50,20 @@ CONFIGURE_FLAGS="
     --without-cxx
     --without-tests
     --without-manpages
+    --disable-stripping
     --enable-widec"
 
-# ncurses wants --with-build-cc when cross-compiling.
+# ncurses wants --with-build-cc when cross-compiling. But it insists on CC
+# and this value not being equal, even though using the same binary with
+# different compiler flags is doable!
 if [[ "${BUILD_TRIPLE}" != "${TARGET_TRIPLE}" && "${PYBUILD_PLATFORM}" != "macos" ]]; then
-  CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-build-cc=${TOOLS_PATH}/${TOOLCHAIN}/bin/clang"
+  # Look for and use our Clang toolchain by default. If not present, fall
+  # back to likely path to system GCC.
+  if [ -e "${TOOLS_PATH}/${TOOLCHAIN}/bin/clang" ]; then
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-build-cc=${TOOLS_PATH}/${TOOLCHAIN}/bin/clang"
+  else
+    CONFIGURE_FLAGS="${CONFIGURE_FLAGS} --with-build-cc=/usr/bin/x86_64-linux-gnu-gcc"
+  fi
 fi
 
 if [ "${PYBUILD_PLATFORM}" = "macos" ]; then
