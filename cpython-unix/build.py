63ac3a61c4c6fbcb779044537e31968e9af3b8bd
Gregory Szorc <gregory.szorc@gmail.com>
2021-03-01 12:59:17 -0800

    unix: move Apple cflags to YAML
    Part of moving static configuration out of code.

--- cpython-unix/build.py
+++ cpython-unix/build.py
@@ -127,21 +127,6 @@ def add_target_env(env, build_platform, target_triple, build_env):
         # non-system (e.g. Homebrew) executables from being used.
         env["PATH"] = "/usr/bin:/bin"
 
-        extra_target_cflags.extend(
-            [
-                # Suppress extremely verbose warnings we see with LLVM 10.
-                "-Wno-nullability-completeness",
-                "-Wno-expansion-to-defined",
-                # LLVM 11 contains commit https://reviews.llvm.org/D83250,
-                # which enables -Werror for undef-prefix=TARGET_OS_.
-                # However, the macOS SDK has headers that reference deprecated
-                # TARGET_OS defines, like TARGET_OS_EMBEDDED. So LLVM 11 refuses
-                # to work with the macOS SDKs out of the box. We work around
-                # this by undoing the -Werror=undef-prefix in that commit.
-                "-Wno-undef-prefix",
-            ]
-        )
-
         if "APPLE_SDK_PATH" in os.environ:
             sdk_path = os.environ["APPLE_SDK_PATH"]
         else:
