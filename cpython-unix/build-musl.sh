5f9021eb7a3273fdbd3606ac78383a810cedd76a
Gregory Szorc <gregory.szorc@gmail.com>
2021-05-01 08:46:06 -0700

    unix: disable reallocarray() in musl
    musl 1.2.2 introduced reallocarray(), which appears to be used by
    at least OpenSSL.
    
    This poses problems for Linux distributions not running musl 1.2.2, such
    as Debian Bullseye (and earlier of course).
    
    Because we want our Python distributions to be relinkable on as
    many machines as possible, this commit removes reallocarray() from
    our musl build. This should hopefully enable Linux distros running
    an older musl to link our Python distributions.
    
    This is related to https://github.com/indygreg/PyOxidizer/issues/392.

--- cpython-unix/build-musl.sh
+++ cpython-unix/build-musl.sh
@@ -14,6 +14,44 @@ tar -xf musl-${MUSL_VERSION}.tar.gz
 
 pushd musl-${MUSL_VERSION}
 
+# Debian as of at least bullseye ships musl 1.2.1. musl 1.2.2
+# added reallocarray(), which gets used by at least OpenSSL.
+# Here, we disable this single function so as to not introduce
+# symbol dependencies on clients using an older musl version.
+patch -p1 <<EOF
+diff --git a/include/stdlib.h b/include/stdlib.h
+index b54a051f..194c2033 100644
+--- a/include/stdlib.h
++++ b/include/stdlib.h
+@@ -145,7 +145,6 @@ int getloadavg(double *, int);
+ int clearenv(void);
+ #define WCOREDUMP(s) ((s) & 0x80)
+ #define WIFCONTINUED(s) ((s) == 0xffff)
+-void *reallocarray (void *, size_t, size_t);
+ #endif
+ 
+ #ifdef _GNU_SOURCE
+diff --git a/src/malloc/reallocarray.c b/src/malloc/reallocarray.c
+deleted file mode 100644
+index 4a6ebe46..00000000
+--- a/src/malloc/reallocarray.c
++++ /dev/null
+@@ -1,13 +0,0 @@
+-#define _BSD_SOURCE
+-#include <errno.h>
+-#include <stdlib.h>
+-
+-void *reallocarray(void *ptr, size_t m, size_t n)
+-{
+-	if (n && m > -1 / n) {
+-		errno = ENOMEM;
+-		return 0;
+-	}
+-
+-	return realloc(ptr, m * n);
+-}
+EOF
+
 ./configure \
     --prefix=/tools/host \
     --disable-shared
