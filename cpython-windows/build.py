9de5fd330e798ae8b4d661983e0320f1a8780f10
Gregory Szorc <gregory.szorc@gmail.com>
2022-02-27 11:18:36 -0700

    windows: properly test against Python version string
    The value passed in here is `XY` not `X.Y`. That means that our
    `pathcch` and `shlwapi` annotations were incorrect prior to this
    change.
    
    This fixes bugs in the following commits:
    
    * 9fb81f1015ea3846595599d83adbe6be5262630b
    * cc9da9c3f9bd2d3c6e5c72765053b04828c87ece
    * 4ff995a0b247c5a70cd99a6a707324ecb0c9c245

--- cpython-windows/build.py
+++ cpython-windows/build.py
@@ -1876,11 +1876,11 @@ def collect_python_build_artifacts(
     ]
 
     # pathcch is required on 3.9+ and its presence drops support for Windows 7.
-    if python_majmin != "3.8":
+    if python_majmin != "38":
         res["core"]["links"].append({"name": "pathcch", "system": True})
 
     # shlwapi was dropped from 3.9.9+.
-    if python_majmin == "3.8":
+    if python_majmin == "38":
         res["core"]["links"].append({"name": "shlwapi", "system": True})
 
     # Copy files for extensions into their own directories.
