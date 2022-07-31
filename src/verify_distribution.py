90332afbf9c4dd86f1c9350f4b23c58d51885ff5
Gregory Szorc <gregory.szorc@gmail.com>
2022-07-31 12:10:12 -0700

    downloads: upgrade OpenSSL from 1.1.1p to 1.1.1q

--- src/verify_distribution.py
+++ src/verify_distribution.py
@@ -127,7 +127,7 @@ class TestPythonInterpreter(unittest.TestCase):
         self.assertTrue(ssl.HAS_TLSv1_2)
         self.assertTrue(ssl.HAS_TLSv1_3)
 
-        self.assertEqual(ssl.OPENSSL_VERSION_INFO, (1, 1, 1, 16, 15))
+        self.assertEqual(ssl.OPENSSL_VERSION_INFO, (1, 1, 1, 17, 15))
 
         ssl.create_default_context()
 
