c0d1fbbc91e192da0ac4ec7fb2b5f6cc2fd80068
Gregory Szorc <gregory.szorc@gmail.com>
2023-04-06 22:53:59 -0700

    unix: upgrade LLVM 15.0.7 -> 16.0.2

--- pythonbuild/utils.py
+++ pythonbuild/utils.py
@@ -395,7 +395,7 @@ def clang_toolchain(host_platform: str, target_triple: str) -> str:
         if "musl" in target_triple:
             return "llvm-14-x86_64-linux"
         else:
-            return "llvm-15-x86_64-linux"
+            return "llvm-16-x86_64-linux"
     elif host_platform == "macos":
         if platform.mac_ver()[2] == "arm64":
             return "llvm-aarch64-macos"
