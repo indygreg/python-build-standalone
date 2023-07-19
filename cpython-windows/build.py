0248aae5848638bccc2ed0c0adb852adc63e640f
Gregory Szorc <gregory.szorc@gmail.com>
2023-07-18 18:39:19 -0700

    global: default to building `cpython-3.11`
    The .4 release is out and 3.11 is pretty stable. It is time to
    make it the default.

--- cpython-windows/build.py
+++ cpython-windows/build.py
@@ -2608,7 +2608,7 @@ def main():
     parser.add_argument(
         "--python",
         choices={"cpython-3.8", "cpython-3.9", "cpython-3.10", "cpython-3.11"},
-        default="cpython-3.10",
+        default="cpython-3.11",
         help="Python distribution to build",
     )
     parser.add_argument(
