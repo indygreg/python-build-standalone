b0f8d6d6fec5eac980ce0bc19b34672ce6b24d56
Gregory Szorc <gregory.szorc@gmail.com>
2023-08-24 20:28:31 -0700

    downloads: openssl 1.1.1u -> 1.1.1v

--- src/verify_distribution.py
+++ src/verify_distribution.py
@@ -131,7 +131,7 @@ class TestPythonInterpreter(unittest.TestCase):
         self.assertTrue(ssl.HAS_TLSv1_2)
         self.assertTrue(ssl.HAS_TLSv1_3)
 
-        self.assertEqual(ssl.OPENSSL_VERSION_INFO, (1, 1, 1, 21, 15))
+        self.assertEqual(ssl.OPENSSL_VERSION_INFO, (1, 1, 1, 22, 15))
 
         ssl.create_default_context()
 
