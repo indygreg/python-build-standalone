ea4413dca5aa98fae3e6333a4603c837737aa58c
Gregory Szorc <gregory.szorc@gmail.com>
2024-07-14 11:34:20 -0700

    downloads: libffi 3.4.4 -> 3.4.6
    This release incorporates the path we were applying manually. So stop
    applying it.

--- cpython-unix/build-libffi.sh
+++ cpython-unix/build-libffi.sh
@@ -13,40 +13,6 @@ tar -xf libffi-${LIBFFI_VERSION}.tar.gz
 
 pushd libffi-${LIBFFI_VERSION}
 
-# Upstream commit ce077e5565366171aa1b4438749b0922fce887a4 to resolve a missing declaration.
-patch -p1 << 'EOF'
-diff --git a/include/ffi_common.h b/include/ffi_common.h
-index 2bd31b0..c53a794 100644
---- a/include/ffi_common.h
-+++ b/include/ffi_common.h
-@@ -128,6 +128,10 @@ void *ffi_data_to_code_pointer (void *data) FFI_HIDDEN;
-    static trampoline. */
- int ffi_tramp_is_present (void *closure) FFI_HIDDEN;
-
-+/* Return a file descriptor of a temporary zero-sized file in a
-+   writable and executable filesystem. */
-+int open_temp_exec_file(void) FFI_HIDDEN;
-+
- /* Extended cif, used in callback from assembly routine */
- typedef struct
- {
-diff --git a/src/tramp.c b/src/tramp.c
-index 7e005b0..5f19b55 100644
---- a/src/tramp.c
-+++ b/src/tramp.c
-@@ -39,6 +39,10 @@
- #ifdef __linux__
- #define _GNU_SOURCE 1
- #endif
-+
-+#include <ffi.h>
-+#include <ffi_common.h>
-+
- #include <stdio.h>
- #include <unistd.h>
- #include <stdlib.h>
-EOF
-
 EXTRA_CONFIGURE=
 
 # mkostemp() was introduced in macOS 10.10 and libffi doesn't have
